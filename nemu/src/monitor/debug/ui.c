67076182a6479955bd76f4f5624a094126937872
tracer-ics2020 <tracer@njuics.org>
2023-05-13 14:00:57 +0800

    >  compile 201914673  Static hostname: ysq-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: edfe6041fd1045aba43add31835aba11          Boot ID: 99498d867453446c992f557de005bc1b   Virtualization: vmware Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.19.0-41-generic     Architecture: x86-64  14:00:57 up 1 day, 36 min,  2 users,  load average: 0.13, 0.16, 0.17

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -87,33 +87,34 @@ static int cmd_help(char *args) {
 
 
 static int cmd_si(char *args){
-  char* args_end = args+strlen(args);
+  // char* args_end = args+strlen(args);
   // if (nemu_state.state!=NEMU_RUNNING || nemu_state.state!=NEMU_STOP){   /*程序不在跑*/
   //   printf("The program is not being run.\n");
   //   return 0;
   // }
-  cpu_exec(-1);
-  char *arg = strtok(NULL, " ");
-  if(arg == NULL){                    /*没参数，默认N=1*/
-    return 1;
-  }
-  char* second_args = arg+strlen(arg)+1;
-  if(second_args < args_end){         /*参数多于1个*/
-    printf("A syntax error in expression, near `%s'\n", second_args);
-    return 0;
-  }
-  if(arg[0]<'0' || arg[0]>'9'){       /*非数字符号开头*/ 
-    printf("No symbol \"%s\" in current context.", arg);
-    return 0;
-  }
-  char *str_end=NULL;
-  int step = strtol(arg, &str_end, 0);
-  if(str_end!=arg+strlen(arg)){       /*数字开头的非数*/
-    printf("Invalid number \"%s\".", arg);
-    return 0;
-  }else{                              /*正常*/
-    return step>0?step:0;             // 执行max(0, step)步
-  }
+  cpu_exec(1);
+  return 0;
+  // char *arg = strtok(NULL, " ");
+  // if(arg == NULL){                    /*没参数，默认N=1*/
+  //   return 1;
+  // }
+  // char* second_args = arg+strlen(arg)+1;
+  // if(second_args < args_end){         /*参数多于1个*/
+  //   printf("A syntax error in expression, near `%s'\n", second_args);
+  //   return 0;
+  // }
+  // if(arg[0]<'0' || arg[0]>'9'){       /*非数字符号开头*/ 
+  //   printf("No symbol \"%s\" in current context.", arg);
+  //   return 0;
+  // }
+  // char *str_end=NULL;
+  // int step = strtol(arg, &str_end, 0);
+  // if(str_end!=arg+strlen(arg)){       /*数字开头的非数*/
+  //   printf("Invalid number \"%s\".", arg);
+  //   return 0;
+  // }else{                              /*正常*/
+  //   return step>0?step:0;             // 执行max(0, step)步
+  // }
 }
 
 void ui_mainloop() {
