dc92c208fb79cf4870a1f917ede767936c06c17c
tracer-ics2020 <tracer@njuics.org>
2023-05-14 11:37:57 +0800

    >  compile 201914673  Static hostname: ysq-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: edfe6041fd1045aba43add31835aba11          Boot ID: 99498d867453446c992f557de005bc1b   Virtualization: vmware Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.19.0-41-generic     Architecture: x86-64  11:37:57 up 1 day,  7:15,  2 users,  load average: 0.21, 0.21, 0.19

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -110,12 +110,14 @@ int eval(int p, int q, bool *err) {
     return 0;
   }
   if (p > q) {
+    printf("1\n");
     /* Bad expression */ // 如()
     printf("Bad expression: p=%d, q=%d\n", p, q);
     *err = true;
     return 0;
   }
   else if (p == q) {
+    printf("2\n");
     /* Single token.
      * For now this token should be a number.
      * Return the value of the number.
@@ -129,6 +131,7 @@ int eval(int p, int q, bool *err) {
     return strtol(tokens[p].str, NULL, 0);
   }
   else if (check_parentheses(p, q, err) == true) {
+    printf("3\n");
     /* The expression is surrounded by a matched pair of parentheses.
      * If that is the case, just throw away the parentheses.
      */
@@ -136,6 +139,7 @@ int eval(int p, int q, bool *err) {
     return eval(p + 1, q - 1, err);
   }
   else {
+    printf("4\n");
     if (*err==true) {
       return 0;
     }
